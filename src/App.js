150b6c07e1afb8ef1a6cf871242d75532c756624
Surya T N <suryatej.nim@yahoo.com>
2022-11-14 07:15:11 +0000

    Set width to be 65% for view port widths >= 768px, through a media query in the styled component GameScoreHeaderContainer, which renders the outer container JSX element in GameHeader component

--- src/App.js
+++ src/App.js
@@ -66,6 +66,10 @@ const GameScoreHeaderContainer = styled.div`
   padding: 1rem 2rem;
   width: 100%;
   height: 20vh;
+
+  @media (min-width: 768px) {
+    width: 65%;
+  }
 `
 
 const BrandContainer = styled.div`
